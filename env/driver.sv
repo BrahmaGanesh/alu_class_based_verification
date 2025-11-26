2b870f21082c8fb18851bb75a4e5198126d6c442
BrahmaGanesh <katrapallibrahmaganesh@gmail.com>
2025-11-26 12:19:07 +0530

    Add driver class to drive ALU interface signals using mailbox transactions

---
+++ env/driver.sv
@@ -0,0 +1,24 @@
+class driver;
+    virtual alu_interface.master vif;
+    transaction tr;
+    mailbox mbxgd;
+  
+  function new(virtual alu_interface.master vif,mailbox mbxgd);
+        this.vif    = vif;
+        this.mbxgd  = mbxgd;
+    endfunction
+    
+    task run();
+        forever begin
+            mbxgd.get(tr);
+            
+            vif.cb.operand_a   <= tr.operand_a;
+            vif.cb.operand_b   <= tr.operand_b;
+            vif.cb.opcode      <= tr.opcode;
+            
+            tr.display("DRV");
+            
+          @(posedge vif.cb);
+        end
+    endtask
+endclass
